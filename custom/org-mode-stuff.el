c76e6783f5b6ea0929789e5bbc90d787d4d6a725
Chris Jenkins <cdpjenkins@gmail.com>
2018-04-07 23:46:01 +0100

    Meh

--- custom/org-mode-stuff.el
+++ custom/org-mode-stuff.el
@@ -1,2 +1,3 @@
 ;; org-mode hooks
 (add-hook 'org-mode-hook 'column-number-mode)
+(add-hook 'org-mode-hook 'visual-line-mode)
--- init.el
+++ init.el
@@ -1,10 +1,20 @@
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (cider twittering-mode rspec-mode robe rainbow-delimiters projectile-rails popup paredit magit htmlize highlight-indentation haskell-mode flymake-ruby enh-ruby-mode company clojure-test-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
